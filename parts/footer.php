60f3a3187d257822cec28ec73c59cf28601bdf36
Vanoux <vanessa.roux891@orange.fr>
2018-07-26 13:39:00 +0200

    Etape 2

--- parts/footer.php
+++ parts/footer.php
@@ -1,1 +1,4 @@
 put some footer here
+<?php include "public/app.js"; ?>
+</body>
+</html>
--- parts/header.php
+++ parts/header.php
@@ -1,1 +1,19 @@
 put some header here
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+
+</head>
+
+<body>
+    
+<?php include "menu.php"; ?>
+
+
